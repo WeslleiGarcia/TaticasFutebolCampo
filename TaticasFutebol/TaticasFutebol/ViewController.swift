b9b3cc476073364f7b163deb748bd5377427bb95
Wesllei <Garcia@macbook-pro-de-wesllei.local>
2015-08-17 14:29:51 -0400

    Criado Documento para Inicio do projeto

---
+++ TaticasFutebol/TaticasFutebol/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaticasFutebol
+//
+//  Created by Wesllei on 17/08/15.
+//  Copyright © 2015 Wesllei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
